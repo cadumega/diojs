94439a87fd515c8bc500560432ee0e37d00b0f04
cadumega <72901445+cadumega@users.noreply.github.com>
2021-03-17 02:00:11 -0300

    arrays

---
+++ script.js
@@ -0,0 +1,72 @@
+// Aula 3 Condicionais, laços de repetição e Date
+
+// var idade = prompt("Qual sua idade? ")      //o que responder irá ser gravado na variável
+// if (idade >= 18) {
+//   alert("maior de idade");
+// }else {
+//   alert("menor de idade");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Aula 2 Arrays
+
+// Criando array
+// var lista = ["maça" , "pera" , "banana"]
+// lista.push("uva");           //add element
+// lista.pop();                // delete last element
+
+// var fruta = [{nome:"maça", cor:"vermelha"} , {nome:"uva" , cor:"roxa"}]
+// console.log(fruta);
+// alert(fruta[1].nome);              //position 1 
+
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta);
+// console.log(fruta.nome);
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join( " - "));    //separador por - 
+
+// console.log(lista[0]);
+// console.log(lista.toString()[0]);
+
+// console.log (lista.reverse())
+// console.log (lista.lenght); 
+
+
+
+
+
+
+
+
+// Aula 1 Introdução
+
+// var nome = "Carlos Mega";
+// var idade = 5;
+// var idade2 = 3;
+// var frase = "Japão é o melhor time do mundo";
+// // alert ( nome + " tem " ,idade, + " anos" );
+// alert (idade + idade2)
+
+
+// console.log (nome);
+// console.log (idade + idade2);
+// // console.log (frase.toUppercase());
+// console.log(frase);
+// // console.log (frase.replace("Japão", "Brasil"));
